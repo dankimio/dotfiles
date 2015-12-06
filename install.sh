f24f8bd860ed5acfc800a4a066d7f9817a4b4e8d
Dan K <itsdanya@gmail.com>
2015-12-06 14:58:06 +0300

    Install rbenv plugins and Ruby

--- install.sh
+++ install.sh
@@ -20,4 +20,18 @@ brew install ssh-copy-id
 # Caskroom
 brew install caskroom/cask/brew-cask
 brew cask install atom paw postgres gitup cyberduck
-brew cask install dropbox flux firefox
+brew cask install dropbox flux vlc
+brew cask install heroku-toolbelt
+
+brew tap caskroom/fonts
+brew cask install font-fira-mono font-open-sans
+
+# rbenv plugins
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/garnieretienne/rvm-download.git ~/.rbenv/plugins/rvm-download
+
+# Ruby
+rbenv download 2.2.3
+
+# Generate SSH
+# ssh-keygen
