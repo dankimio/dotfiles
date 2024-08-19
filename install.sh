3ee43083045242dc5451b326fb5145a5caded21b
Dan Kim <itsdanya@gmail.com>
2024-08-19 13:12:57 +0300

    Install oh-my-zsh first and check if Homebrew is installed

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 dir="$HOME/Developer/dotfiles"
 cd $dir
 
+# ohmyzsh
+rm -rf ~/.oh-my-zsh
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 rm -f ~/.zshrc
 
 files="gemrc gitconfig hushlogin irbrc rubocop.yml zshrc"
@@ -11,17 +15,18 @@ for file in $files; do
 done
 
 # Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-exec $SHELL -l;
+if [[ $(command -v brew) == "" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  exec $SHELL -l;
+fi
+
 brew bundle
+exec $SHELL -l;
 
 # Install the latest Ruby version with rbenv
 rbenv install $(rbenv install -l | grep -v - | tail -1)
 rbenv global $(rbenv install -l | grep -v - | tail -1)
 bundle
-
-# ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Generate SSH key
 ssh-keygen -N ''
