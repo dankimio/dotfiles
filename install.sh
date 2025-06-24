46bef3a0a4e3339d7baa2c25c17942dabbd68ebd
Dan Kim <itsdanya@gmail.com>
2025-06-24 03:25:40 +0300

    Init brew and rbenv

--- install.sh
+++ install.sh
@@ -15,17 +15,19 @@ for file in $files; do
 done
 
 # Homebrew
-if [[ $(command -v brew) == "" ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  exec $SHELL -l;
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Make brew immediately available in this shell
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle
-exec $SHELL -l;
 
 # Install the latest Ruby version with rbenv
-rbenv install $(rbenv install -l | grep -v - | tail -1)
-rbenv global $(rbenv install -l | grep -v - | tail -1)
+latest="$(rbenv install -l | grep -v '^-' | tail -1 | tr -d ' ')"
+rbenv install -s "$latest"
+rbenv global "$latest"
 bundle
 
 # Generate SSH key
